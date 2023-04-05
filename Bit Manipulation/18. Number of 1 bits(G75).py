73ff25ec0b6d2212f974a1cf43ec384fd1102383
Sangram Navale <sangramnavale29@gmail.com>
2023-04-05 11:42:11 -0500

    Create 18. Number of 1 bits(G75).py

---
+++ Bit Manipulation/18. Number of 1 bits(G75).py
@@ -0,0 +1,43 @@
+# Number of 1 bits 
+'''
+Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. 
+It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+ 
+
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
+Example 2:
+
+Input: n = 00000000000000000000000010000000
+Output: 1
+Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+
+Example 3:
+
+Input: n = 11111111111111111111111111111101
+Output: 31
+Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+'''
+
+def HammingWeight(n):
+  count = 0
+  while(n):
+    n = n & (n - 1)
+    count = count + 1
+  return count
+
+h1 = HammingWeight(00000000000000000000000000001011)
+h2 = HammingWeight(00000000000000000000000010000000)
+h3 = HammingWeight(11111111111111111111111111111101)
+print(h1)
+print(h2)
+print(h3)
