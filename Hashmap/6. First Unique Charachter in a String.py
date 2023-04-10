f50c588fb7bd9e8249e3aed8b8c282f79678d17e
Sangram Navale <sangramnavale29@gmail.com>
2023-04-09 23:44:50 -0500

    Create 6. First Unique Charachter in a String.py

--- Hashmap/6. First Unique Charachter in a String.py
+++ Hashmap/6. First Unique Charachter in a String.py
@@ -1,3 +1,31 @@
+'''
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of only lowercase English letters.
+
+
+'''
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         di = {}
