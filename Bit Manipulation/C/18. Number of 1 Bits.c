06ab3008364211f167c319f0ae12933018ae9f6e
Sangram Navale <sangramnavale29@gmail.com>
2023-09-08 11:09:23 -0500

    Update 18. Number of 1 Bits.c

--- Bit Manipulation/C/18. Number of 1 Bits.c
+++ Bit Manipulation/C/18. Number of 1 Bits.c
@@ -3,9 +3,9 @@
 int hammingWeight(uint_t n)
 {  
   int count = 0;
-  while(n > 0)
+  while (n > 0)
   {
-    n = n & (n - 1);     // n - 1 : Flips the rightmost set bit to 0 and the bits to it's right to 1. n = n & (n -1): Will give us a new result 
+    n = n & (n - 1); // n - 1: Flips the rightmost set bit to 0 and the bits to its right to 1. n = n & (n - 1): Will give us a new result 
     count++;
   }
   return count;
@@ -13,9 +13,10 @@ int hammingWeight(uint_t n)
 
 int main()
 {
-  printf("Enter a value for n: );
-  scanf("%d", &n);
+  uint_t n; // Declare n
+  printf("Enter a value for n: "); // Added a prompt
+  scanf("%u", &n); // Use %u for an unsigned integer
   int count = hammingWeight(n);
-  printf("No. of 1 bits is: %d, count);
+  printf("No. of 1 bits is: %d\n", count); // Corrected the format string and added \n
   return 0;
 }
