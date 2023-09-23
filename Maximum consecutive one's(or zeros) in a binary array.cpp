39477eecee880c4260fdaa7f1f2950407b6d8c7d
Sangram Navale <sangramnavale29@gmail.com>
2023-09-23 14:13:15 -0500

    Update Maximum consecutive one's(or zeros) in a binary array.cpp

--- Maximum consecutive one's(or zeros) in a binary array.cpp
+++ Maximum consecutive one's(or zeros) in a binary array.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int maxConsOne(int nums[], int numsSize)
+int maxConsOne(vector<int> &nums)
 {
     //..keep a track of max_count and curr_count of 1s (or 0s as per qsn)
     //..update max count by making max(max_count, curr_count)
     //..if you encounter a 0(or a 1, then make curr count = 0)
     int max_cnt = 0;
     int curr_cnt = 0;
-    for(int i = 0; i < numsSize; i++)
+    for(int i = 0; i < nums.size(); i++)
     {
         if(nums[i] == 1)
         {
@@ -26,9 +27,8 @@ int maxConsOne(int nums[], int numsSize)
 
 int main()
 {
-    int nums[] = {1,1,0,0,1,0,1,0,1,1,1,1};
-    int numsSize = sizeof(nums)/sizeof(nums[0]);
-    int current_count = maxConsOne(nums, numsSize);
+    vector<int> nums = {1,1,0,0,1,0,1,0,1,1,1,1};
+    int current_count = maxConsOne(nums);
     cout << "Number of consecutive 1's is: " << current_count << endl;
     return 0;
 }
